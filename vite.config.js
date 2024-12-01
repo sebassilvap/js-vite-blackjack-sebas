754f4acdc2bd6cb8bbddea1e6c13d06a8838c6f6
Sebastian Silva <sebas.silva.p@gmail.com>
2024-12-01 16:56:18 +0100

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// archivo de como queremos configurar Vite
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	// añadir plugis, cambiar directorio público
+	// para realizar cambios
+	base: '/js-vite-blackjack-sebas/',
+});
